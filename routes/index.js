56fc6ea1da9bee89099c5a3643017453c361de87
shivanshguleria <shivanshguleria@proton.me>
2023-03-30 19:47:03 +0530

    1.2.1 Adding pug as template engine Deleted static pages

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,26 @@ var router = express.Router();
 const path = require('path');
 
 
- //GET home page. 
+//GET home page. 
 router.get('/', function(req, res) {
-  res.sendFile(path.resolve('public/pages/index.html'));
-  console.log(req.ip);
+  res.render('index', {title: 'Shivansh Guleria'});
 });
 
 //GET about
 router.get('/about', function(req, res) {
-  res.sendFile(path.resolve('public/pages/about.html'));
+  res.render('about', {title: 'About'});
 });
+
+//GET 404
+router.get('/404', function(req, res){
+  res.render('404', {title: 'Page Not Found'});
+});
+
+//GET test
+router.get('/test',function(req, res){
+  res.render('test', {title : '!!This is a Test!!'})
+})
+
 //GET resume
 router.get('/resume', function(req, res) {
   res.sendFile(path.resolve('public/resume.pdf'));
@@ -22,14 +32,5 @@ router.get('/resume', function(req, res) {
 router.get('/sitemap', function(req, res) {
   res.sendFile(path.resolve('public/sitemap.xml'));
 });
-
-//GET 404
-router.get('/404', function(req, res){
-  res.sendFile(path.resolve('public/pages/404.html'));
-});
-//GET font test
-router.get('/font-test',function(req, res){
-  res.sendFile(path.resolve('public/pages/font-test.html'))
-})
 
 module.exports = router;
