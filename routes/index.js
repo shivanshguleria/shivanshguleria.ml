1b3c05e4ada2b5d9762ebb79be5c7e7c0cf57ba3
shivansh <shivansh.guleria@yahoo.com>
2023-06-08 00:15:12 +0530

    1.4.2-2 -Fixed proxy error

--- routes/index.js
+++ routes/index.js
@@ -84,8 +84,7 @@ router.post('/upload', (req,res) => {
   res.json({message: "Files Added"});
 })
 }
-else{                 
-  
+else{
   router.get('/files/upload', (req, res) => {
     res.redirect("https://www.youtube.com/watch?v=H8ZH_mkfPUY");
   });
