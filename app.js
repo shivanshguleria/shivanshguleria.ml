0d02df63493af13902cf32f5c6cc17ad4fd38394
Shivansh Guleria <shivanshguleria@proton.me>
2023-09-11 09:16:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Pages
-app.get('/', (req,res) => {res.send("Site is down \\n May be down for longtime\\n Meanwhile, visit archived page \\nhttps://web.archive.org/web/20230106120447/https://shivanshguleria.ml/")});
+app.get('/', (req,res) => {res.send("<h4>Site is down <br> Meanwhile, visit <br><a href="https://web.archive.org/web/20230106120447/https://shivanshguleria.ml/">archived page</a></h4>")});
 
 
 //404
